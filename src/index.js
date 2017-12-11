c8e8fc2993a1322169c5889ef5178a36cb2db556
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-12-11 00:37:32 -0200

    Thunk

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//redux
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+//importa o agrupador de 
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
