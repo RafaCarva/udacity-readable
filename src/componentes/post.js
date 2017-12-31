680935b64a1bf471568da1d439e0eb752c3e32f4
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-12-31 17:33:31 -0200

    deletar post funcionando

--- src/componentes/post.js
+++ src/componentes/post.js
@@ -1,42 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { deletarPosts } from '../actions/actionPosts'
+
 import './post.css'
 
-const post = props => {
-  const {posts} = props;
+class post extends Component {
 
-  const excluirPost = id => {
+  render() {
+    const { posts } = this.props;
 
-    axios
-    .delete ('http://localhost:3001/posts/'+id, {
-      headers: {
-        Authorization: 'whatever-you-want'
-      },
-    })
-    .then (response => {
-      console.log("post id:",id," deletado!");
-    })
-    .catch (error => {
-      console.log ('ERRO no delete do post', error);
-    });
+    const excluirPost = id => {
+
+      axios
+        .delete('http://localhost:3001/posts/' + id, {
+          headers: {
+            Authorization: 'whatever-you-want'
+          },
+        })
+        .then(response => {
+          console.log("post id:", id, " deletado!");
+          //deletarPosts é o action que foi importado
+          //e associado ao dispatch
+          this.props.deletarPosts(id);
+        })
+        .catch(error => {
+          console.log('ERRO no delete do post', error);
+        });
+    }
+
+    return (
+      <ul className="postLista">
+        {posts.map((link, key) => (
+          <li key={key} className="postContainer">
+            {/*<p>id:{link.id}</p>*/}
+            titulo:{link.title}
+            <br />
+            category:{link.category}
+            <br />
+            <button onClick={() => excluirPost(link.id)}>deletar post</button>
+
+          </li>
+        ))}
+      </ul>
+    )//return
+  }//render
 
 
-  }
+}//class
 
-  return (
-    <ul className="postLista">
-      {posts.map ((link, key) => (
-        <li key={key} className="postContainer">
-          {/*<p>id:{link.id}</p>*/}
-          titulo:{link.title}
-          <br />
-          category:{link.category}
-          <br />
-          <button onClick={() => excluirPost (link.id)}>deletar post</button>
+function mapStateToProps(state) {
+  return {
+    ...state.ReducerPosts,
+    ...state,
+  };
+}
 
-        </li>
-      ))}
-    </ul>
-  );
-};
-export default post;
+const mapDispatchToProps = dispatch => bindActionCreators({
+  deletarPosts
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(post);
--- src/reducers/reducerPosts.js
+++ src/reducers/reducerPosts.js
@@ -8,6 +8,18 @@ export default function (state = getInitialState, action) {
       return {
         posts:action.payload
       }
+
+      /**
+       * action.payload é o ID.
+       * O posts vai receber o resultado do filtro de id's
+       * todos os que forem diferentes do payload.
+       */
+      case 'DELETAR-POSTS':
+      return {
+        posts: [...state.posts.filter((element, index, arr)=>
+          element.id !== action.payload
+        )]
+      }
      
       default: return state
   }
