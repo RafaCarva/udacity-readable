0f30a8bbf3e9aacf3a2365b05b2de119d67ae62a
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-01-09 23:25:13 -0200

    post detalhado

--- src/componentes/postDetalhado.js
+++ src/componentes/postDetalhado.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Post from './post'
+import axios from 'axios';
 
+class postDetalhado extends Component {
 
-class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ id: this.props.match.params.id });
+  }
+
+  componentDidMount() {
+    // pegar o post
+    axios
+      .get(`http://localhost:3001/posts/${this.state.id}`, {
+        headers: { Authorization: 'whatever-you-want' },
+      })
+      .then(response => {
+        console.log('then(response ->', response.data);
+
+        //por o post no state
+        this.setState({ posts: response.data });
+      })
+      .catch(error => { console.log('ERRO', error); });
+  }
+
   render() {
+
+  console.log('render()-> this.state.post->',this.state.post);
+    //const {posts} = this.state.post;
+    //console.log('{posts}->',posts);
     return (
       <div>
-        
+
 
         <h1>post detalhado</h1>
+       {/* <Post posts={} /> */}
       </div>
     );
   }
 }
 
-export default componentName;
+function mapStateToProps(state) {
+  return { ...state }
+}
+
+export default connect(mapStateToProps)(postDetalhado);
+
+
