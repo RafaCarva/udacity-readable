893f265e6b7c520974a03d5d5742240f7c22385e
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-01-12 20:36:38 -0200

    listar por /categoria está arrumado

--- src/containers/categoria.js
+++ src/containers/categoria.js
@@ -1,55 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 //componentes
 import Posts from '../componentes/post';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 class categoria extends Component {
-  constructor (props) {
-    super (props);
-    this.state={
-      seletorDePosts:[]
+  constructor(props) {
+    super(props);
+    this.state = {
+      seletorDePosts: []
     }
   }
-  componentDidMount(){
-    
-        axios
-        .get ('http://localhost:3001/posts', {
-          headers: {Authorization: 'whatever-you-want'},
-        })
-        .then (response => {
-          let temp = response.data;
-          let temp2 = temp.filter (item => item.category === 'redux');
-          this.setState(
-            {seletorDePosts:temp2}
-          );
-   
-    
-        })
-        .catch (error => {
-          console.log ('ERRO', error);
-        });
-    
-       }
-  render () {
-   
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/posts', {
+        headers: { Authorization: 'whatever-you-want' },
+      })
+      .then(response => {
+        let temp = response.data;
+        let temp2 = temp.filter(item => item.category == this.props.ReducerCategorias.categoriaAtual);
+        this.setState(
+          { seletorDePosts: temp2 }
+        );
+
+      })
+      .catch(error => {
+        console.log('ERRO', error);
+      });
+
+  }
+  render() {
+
     return (
       <div>
         <Link to="/"> Voltar</Link>
-        <h1>redux</h1>
+        <h1>{this.props.ReducerCategorias.categoriaAtual}</h1>
         <Posts posts={this.state.seletorDePosts} />
       </div>
     );
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
-    ...state.ReducerCategorias,
-    ...state.ReducerPosts,
     ...state,
   };
 }
 
-export default connect (mapStateToProps) (categoria);
+export default connect(mapStateToProps)(categoria);
